6797434b6f11345a9d82d4c31f3400de57777cf9
David <davidmv1185@gmail.com>
2024-01-28 22:26:41 -0800

    setting up table

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-
-    <div>
-
-    </div>
-
-
+    <>
+      <ContactList/>
+    </>
   )
 }
 
